e15c123136ab798f623d69e754ff7f5b16dbb39c
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-20 17:42:05 +0330

    add timer

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import ( 
 	"os"
-	"os/exec"
 	"log"
 	"fmt"
+	"time"
+	"os/exec"
 	"encoding/csv"
 )
 
@@ -27,21 +28,35 @@ func main() {
 		log.Fatal(err)
 	}
 
+	timeLimit := 3
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
 	problems :=  getProblems(records)
-	
 	score := 0
+
 	for i, problem := range problems { 
 
 		clearScreen() 
 		fmt.Printf("Q %d: %s? ", i, problem.question)
-		
-		var ans string 
-		fmt.Scan(&ans)
 
-		if ans == problem.answer{ 
-			score += 1 
+		answerCh := make(chan string)
+		go func() {
+				var ans string 
+				fmt.Scan(&ans)
+				answerCh <- ans
+		}() 
+
+		select { 
+			case <-timer.C :
+				fmt.Printf("you ran out of time!\n" + 
+				"score %d out of %d", score, len(problems))
+				return 
+			case ans := <-answerCh :
+				if ans == problem.answer { 
+					score += 1 
+				}			
 		}
 	}
+	fmt.Printf("score %d out of %d", score, len(problems))
 }
 
 
