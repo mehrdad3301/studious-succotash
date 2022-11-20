f5ba23bdf4e62ef43be4a1ca903cf73498436b05
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-20 18:54:22 +0330

    rename package

--- urlshort/main.go
+++ urlshort/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gophercises/urlshort"
+	"github.com/mehrdad3301/studious-succotash/urlshort"
 )
 
 func main() {
@@ -13,26 +12,25 @@ func main() {
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2", }
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	_ = mapHandler
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
-	_ = yamlHandler
+	//yaml := `
+//- path: /urlshort
+//  url: https://github.com/gophercises/urlshort
+//- path: /urlshort-final
+//  url: https://github.com/gophercises/urlshort/tree/solution
+//`
+//	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+//	if err != nil {
+//		panic(err)
+//	}
+//	_ = yamlHandler
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
