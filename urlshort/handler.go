bfbbdcf2862b16eb00b8658dde0c158bb829299b
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-20 21:57:04 +0330

    add yaml handler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,14 +2,9 @@ package urlshort
 
 import (
 	"net/http"
+	"github.com/go-yaml/yaml"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) { 
 		path := r.URL.Path 
@@ -20,23 +15,38 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+type pathUrl struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"` 
 }
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc) {
+
+	parsedYaml := make([]pathUrl, 0)
+	yaml.Unmarshal(yml, &parsedYaml)	
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback)
+}
+
+func buildMap(pathUrls []pathUrl) (map[string]string) { 
+
+	m := make(map[string]string)
+	for _, pu := range pathUrls { 
+		m[pu.Path] = pu.Url	
+	}
+	return m
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
