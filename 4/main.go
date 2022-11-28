43c0965d167ea7db1e2dbb701176017637130629
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-28 19:22:37 +0330

    fix bug in main

--- 4/main.go
+++ 4/main.go
@@ -2,6 +2,7 @@ package main
 
 import ( 
 	"fmt"
+	"os"
 	"flag"
 	"github.com/mehrdad3301/studious-succotash/4/link"
 )
@@ -11,7 +12,7 @@ func main() {
 	name := flag.String("n", "ex1.html", "html file name")
 	flag.Parse()
 
-	file, _:= os.Open(*name)
+	file, err := os.Open(*name)
 	if err != nil { 
 		fmt.Println(err)
 	}
