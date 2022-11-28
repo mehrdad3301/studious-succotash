62aa7a060d33aab10afe613b74869827c49eccb4
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-11-28 17:40:07 +0330

    finish returning hrefs

---
+++ 4/main.go
@@ -0,0 +1,53 @@
+package main 
+
+import ( 
+	"os"
+	"fmt"
+	"flag"
+	"golang.org/x/net/html"
+)
+
+type Link struct { 
+	Href string 
+	Text string 
+}
+
+func GetLinks(n *html.Node, links *[]Link) { 
+	
+	if n.Type == html.ElementNode { 
+		if n.Data == "a" { 
+			l := Link{ Href: getHref(n.Attr) } 
+			*links = append(*links, l)
+			return 
+		}
+	}
+
+	for c := n.FirstChild ; c != nil ; c = c.NextSibling { 
+		GetLinks(c, links)
+	}
+}
+
+func getHref(attr []html.Attribute) string {
+	for _, a := range attr { 
+		if a.Key == "href" { 
+			return a.Val
+		}
+	}
+	return ""
+}
+
+func main() { 
+	
+	name := flag.String("n", "ex1.html", "html file name")
+	flag.Parse()
+
+	file, _:= os.Open(*name)
+	r, err := html.Parse(file)
+	if err != nil { 
+		fmt.Println(err)
+	}
+
+	var links []Link
+	GetLinks(r, &links)	
+	fmt.Println(links)
+}
